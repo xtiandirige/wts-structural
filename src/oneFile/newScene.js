8f463423c742bb7f4ed0282786b5ce43718a513d
Edgar Dirige <xtiandirige@gmail.com>
2022-06-24 22:08:07 +0800

    test 10

--- src/oneFile/newScene.js
+++ src/oneFile/newScene.js
@@ -286,6 +286,89 @@ class Renderer {
     }
 }
 
+const structureMaterial = new THREE.MeshBasicMaterial({
+    color: '#D3D3D3',
+    transparent: true,
+})
+const supportMaterial = new THREE.MeshBasicMaterial({
+    color: '#D3D3D3',
+    transparent: true,
+});
+
+class Support {
+    constructor(geometry, material, position) {
+        this.geometry = geometry
+        this.material = material
+        this.position = position
+        this.createSupport()
+    }
+
+    createSupport() {
+        this.support = new THREE.Mesh(this.geometry, this.material)
+        this.support.position.set(this.position.x, this.position.y, this.position.z)
+    }
+}
+
+class Roller extends Support {
+    constructor(parameters, radius = 0.25) {
+        if (parameters === undefined) parameters = {}
+        super(
+            new THREE.SphereGeometry(radius),
+            parameters.hasOwnProperty("material") ? parameters["material"] : new THREE.MeshBasicMaterial({ color: 0x000000 }),
+            parameters.hasOwnProperty("position") ? parameters["position"] : new THREE.Vector3(0, 0, 0)
+        )
+    }
+}
+
+class Hinge extends Support {
+    constructor(parameters, radiusTop = 0.3, radiusBottom = 0.6, height = 0.5) {
+        if (parameters === undefined) parameters = {}
+        super(
+            new THREE.CylinderGeometry(radiusTop / Math.sqrt(2), radiusBottom / Math.sqrt(2), height, 4, 1),
+            parameters.hasOwnProperty("material") ? parameters["material"] : new THREE.MeshBasicMaterial({ color: 0x000000 }),
+            parameters.hasOwnProperty("position") ? parameters["position"] : new THREE.Vector3(0, 0, 0)
+        )
+
+    }
+
+    createSupport() {
+        this.support = new THREE.Mesh(this.geometry, this.material)
+        this.support.geometry.rotateY(Math.PI / 4)
+        this.support.position.set(this.position.x, this.position.y, this.position.z)
+    }
+}
+
+class Fixed extends Support {
+    constructor(parameters, width = 0.3, height = 0.6, depth = 0.3) {
+        if (parameters === undefined) parameters = {}
+        super(
+            new THREE.BoxGeometry(width, height, depth),
+            parameters.hasOwnProperty("material") ? parameters["material"] : new THREE.MeshBasicMaterial({ color: 0x000000 }),
+            parameters.hasOwnProperty("position") ? parameters["position"] : new THREE.Vector3(0, 0, 0)
+        )
+    }
+}
+
+class Structure {
+    constructor() {
+        this.beam = new THREE.Mesh(new THREE.BoxGeometry(9, 0.5, 0.3), structureMaterial)
+        this.roller = new Roller({
+            material: supportMaterial,
+            position: new THREE.Vector3(-2.5, -0.5, 0)
+        })
+        this.hinge = new Hinge({
+            material: supportMaterial,
+            position: new THREE.Vector3(2.5, -0.5, 0)
+        })
+        this.getStructure()
+    }
+
+    getStructure() {
+        this.structure = new THREE.Group()
+        this.structure.add(this.beam, this.roller.support, this.hinge.support)
+    }
+}
+
 class World {
     constructor() {
         this.experience = new Experience()
