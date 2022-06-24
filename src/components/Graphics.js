5a0f8cd99ee7864fe1a6a7ea7d16a48909d5a068
Edgar Dirige <xtiandirige@gmail.com>
2022-06-24 10:41:36 +0800

    test commit 4

--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer'
+import katex from 'katex'
 
 class Graphics {
     constructor(graphicParameters = {}, labelParameters = {}) {
