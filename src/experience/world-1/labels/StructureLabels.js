182e763b736330beb9e44dc285bae0fa37014fc0
Edgar Dirige <xtiandirige@gmail.com>
2022-06-24 11:36:15 +0800

    test 5

--- src/experience/world-1/labels/StructureLabels.js
+++ src/experience/world-1/labels/StructureLabels.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { Graphics, makeElement } from '../../../components/Graphics'
+import { makeElement } from '../../../components/Graphics'
 import Experience from '../../Experience'
 
 export default class StructureLabels {
