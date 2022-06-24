91d2a593d5479450ce138a8d7708d497162793f4
Edgar Dirige <xtiandirige@gmail.com>
2022-06-24 10:22:16 +0800

    test-commit 2

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -8,75 +8,78 @@ module.exports = {
     output:
     {
         hashFunction: 'xxhash64',
-        filename: 'bundle.[contenthash].js',
+        // filename: 'bundle.[contenthash].js',
+        filename: 'wts-structural.js',
+        library: "wts-structural",
+        libraryTarget: "umd",
         path: path.resolve(__dirname, '../dist')
     },
     devtool: 'source-map',
     plugins:
-    [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static') }
-            ]
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        }),
-        new MiniCSSExtractPlugin()
-    ],
+        [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, '../static') }
+                ]
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, '../src/index.html'),
+                minify: true
+            }),
+            new MiniCSSExtractPlugin()
+        ],
     module:
     {
         rules:
-        [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use:
-                [
-                    'html-loader'
-                ]
-            },
-
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
-            },
-
-            // CSS
-            {
-                test: /\.css$/,
-                use:
-                [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                type: 'asset/resource',
-                generator:
+            [
+                // HTML
                 {
-                    filename: 'assets/images/[hash][ext]'
-                }
-            },
+                    test: /\.(html)$/,
+                    use:
+                        [
+                            'html-loader'
+                        ]
+                },
 
-            // Fonts
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                type: 'asset/resource',
-                generator:
+                // JS
                 {
-                    filename: 'assets/fonts/[hash][ext]'
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use:
+                        [
+                            'babel-loader'
+                        ]
+                },
+
+                // CSS
+                {
+                    test: /\.css$/,
+                    use:
+                        [
+                            MiniCSSExtractPlugin.loader,
+                            'css-loader'
+                        ]
+                },
+
+                // Images
+                {
+                    test: /\.(jpg|png|gif|svg)$/,
+                    type: 'asset/resource',
+                    generator:
+                    {
+                        filename: 'assets/images/[hash][ext]'
+                    }
+                },
+
+                // Fonts
+                {
+                    test: /\.(ttf|eot|woff|woff2)$/,
+                    type: 'asset/resource',
+                    generator:
+                    {
+                        filename: 'assets/fonts/[hash][ext]'
+                    }
                 }
-            }
-        ]
+            ]
     }
 }
